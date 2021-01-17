38d81859976caf34465eb2808f5691a5abc32e48
Cryton Andrew <crytonadrew21@gmail.com>
2021-01-17 08:28:27 +0200

    Update user route backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,19 +5,28 @@ import {
     registerUser, 
     updateUserProfile, 
     getUsers, 
-    deleteUser
+    deleteUser,
+    getUserById,
+    updateUser
 } from '../controllers/userController.js'
 
 import {protect, admin} from "../middleware/authMiddleware.js"
 const router = express.Router()
 
-router.route("/").post(registerUser).get(protect, admin ,getUsers)
+router.route("/")
+    .post(registerUser)
+    .get(protect, admin ,getUsers)
 
 router.post('/login', authUser)
 
 // We add the protect middleware that is gonna run everytime we hit this route
-router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route("/profile")
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile)
 
-router.route("/:id").delete(protect, admin, deleteUser)
+router.route("/:id")
+    .delete(protect, admin, deleteUser)
+    .get(protect, admin, getUserById)
+    .put(protect, admin, updateUser)
 
 export default router
