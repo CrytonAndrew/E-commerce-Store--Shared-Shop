788d81faa299f1598ff536e7bc2bc7416cdbf23a
Cryton Andrew <crytonadrew21@gmail.com>
2021-01-17 07:37:02 +0200

    Backend, delete url

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,13 @@
 import express from 'express'
-import {authUser, getUserProfile, registerUser, updateUserProfile, getUsers} from '../controllers/userController.js'
+import {
+    authUser, 
+    getUserProfile, 
+    registerUser, 
+    updateUserProfile, 
+    getUsers, 
+    deleteUser
+} from '../controllers/userController.js'
+
 import {protect, admin} from "../middleware/authMiddleware.js"
 const router = express.Router()
 
@@ -10,5 +18,6 @@ router.post('/login', authUser)
 // We add the protect middleware that is gonna run everytime we hit this route
 router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile)
 
+router.route("/:id").delete(protect, admin, deleteUser)
 
 export default router
