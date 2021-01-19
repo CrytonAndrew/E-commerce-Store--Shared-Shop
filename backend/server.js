59b382beb34dfbf06e2d382683d4f057acec9065
Cryton Andrew <crytonadrew21@gmail.com>
2021-01-19 08:44:34 +0200

    Upload folder setup, static folder

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 //We use common JS below
 //Using ES modules
+import path from 'path'
 import express from 'express'
 import dotenv from "dotenv"
 import colors from 'colors'
@@ -9,6 +10,7 @@ import {notFound, errorHandler} from "./middleware/errorMiddleware.js"
 import productRoutes from "./routes/productRouters.js"
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from "./routes/orderRoutes.js"
+import uploadRoutes from "./routes/uploadRoutes.js"
 
 dotenv.config()
 
@@ -26,8 +28,15 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
+app.use("/api/upload", uploadRoutes)
 
 app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+
+// The uploads folder is not accessible by defualt,
+// so it needs to be stack for it to be read by browser
+__dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
