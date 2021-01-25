8e8ca8f09caa96749e7f25411e4a3e165f4a92b2
Cryton Andrew <crytonadrew21@gmail.com>
2021-01-25 09:26:55 +0200

    Footer Update Part 1

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,11 +5,52 @@ const Footer = () => {
     return (
         <footer>
             <Container>
+            <div className="main-footer">
+                <Row>
+                    <Col >
+                        <h4>Company Name</h4>
+                        
+                    </Col>
+                    <Col >
+                    <h4 className="footer-titles">Account Info</h4>
+                    <div className="lists">
+                        <ul>
+                            <li>Profile</li>
+                            <li>My Cart</li>
+                            <li>My Orders</li>
+                        </ul>
+                    </div>  
+                    </Col>
+                    <Col >
+                    <h4 className="footer-titles">Need Help?</h4>
+                    <div className="lists">
+                        <ul>
+                            <li>Help</li>
+                            <li>Contact Info</li>
+                            <li>Product Requests</li>
+                            <li>Our Warehouse</li>
+                        </ul>
+                    </div>  
+                    </Col>
+                    <Col>
+                    <h4 className="footer-titles">Who is Shared Shop</h4>
+                    <div className="lists">
+                        <ul>
+                            <li>Loren Ipsum</li>
+                            <li>Loren Ipsum</li>
+                            <li>Loren Ipsum</li>
+                            <li>Loren Ipsum</li>
+                            <li>Loren Ipsum</li>
+                        </ul>
+                    </div>   
+                    </Col>
+                </Row>
                 <Row>
                     <Col className="text-center py-3">
                         Copyright &copy; Shared Shop
                     </Col>
                 </Row>
+                </div>
             </Container>
         </footer>
     )
